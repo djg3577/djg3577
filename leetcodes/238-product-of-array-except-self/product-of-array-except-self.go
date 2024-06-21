7f07b7ad2e1a3bc3def77649c36fbf6330b53922
Daniel Guillen <danielguillencoco@yahoo.com>
2024-06-21 12:38:03 -0500

    Time: 40 ms (53.70%) | Memory: 8 MB (80.05%) - LeetSync

---
+++ leetcodes/238-product-of-array-except-self/product-of-array-except-self.go
@@ -0,0 +1,15 @@
+func productExceptSelf(nums []int) []int {
+    res := make([]int, len(nums))
+    prefix := 1
+    for index, number := range nums{
+        res[index] = prefix
+        prefix *= number
+    }
+    postfix := 1
+    for i := len(nums)-1; i >= 0; i-- {
+        res[i] *= postfix
+        postfix *= nums[i]
+    }
+
+    return res
+}
