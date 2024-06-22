c1597022953995058c88b074826e5843f1d9cc53
Daniel Guillen <danielguillencoco@yahoo.com>
2024-06-22 13:42:32 -0500

    Time: 0 ms (100.00%) | Memory: 2.8 MB (100.00%) - LeetSync

--- leetcodes/36-valid-sudoku/valid-sudoku.go
+++ leetcodes/36-valid-sudoku/valid-sudoku.go
@@ -1,27 +1,19 @@
 func isValidSudoku(board [][]byte) bool {
-    rows := [9][9]bool{}
-    cols := [9][9]bool{}
-    grids := [3][3][9]bool{}
+    seen := [3][9][9]bool{} // Combined storage for rows, cols, and grids
 
-    for r:=0; r<9; r++{
-        for c:=0; c<9 ;c++{
-            cell := board[r][c]
-
-            if cell == '.'{
-                continue
+    for r := 0; r < 9; r++ {
+        for c := 0; c < 9; c++ {
+            if cell := board[r][c]; cell != '.' {
+                digit := cell - '1' 
+                
+                if seen[0][r][digit] || seen[1][c][digit] || seen[2][r/3*3+c/3][digit] {
+                    return false
+                }
+                
+                seen[0][r][digit] = true
+                seen[1][c][digit] = true
+                seen[2][r/3*3+c/3][digit] = true
             }
-
-            digit := int(cell) - 49
-
-            if rows[r][digit] || 
-               cols[c][digit] || 
-               grids[r/3][c/3][digit]{
-                return false
-            }
-
-            rows[r][digit] = true
-            cols[c][digit] = true
-            grids[r/3][c/3][digit] = true
         }
     }
     return true
